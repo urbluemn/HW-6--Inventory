5dcf50d9b110c5557500fa7e43edc2aa6a516540
urbluemn <nyaroshko1404@gmail.com>
2022-11-02 15:38:45 +0300

    Add project files.

---
+++ InventoryCS/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryCS
+{
+    internal class Inventory
+    {
+        internal List<Product> Products;
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
---
+++ InventoryCS/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryCS
+{
+    internal class Product
+    {
+        protected string Name;
+        protected string Type;
+        protected int Amount;
+        protected int Price;
+
+       public Product(string name, string type, int amount, int price)
+        {
+            Name = name;
+            Type = type;
+            Amount = amount;
+            Price = price;
+        }
+    }
+}
